a259ab383a0198a8570efe64c0032f737442360b
Henrique Xaud <xxhenriquexaud@gmail.com>
2023-10-21 15:10:32 -0300

    first commit

---
+++ papelaria/src/main/java/com/mycompany/papelaria/Caneta.java
@@ -0,0 +1,35 @@
+package com.mycompany.papelaria;
+
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    
+    void status(){
+        System.out.print("Uma caneta " + this.modelo);
+        System.out.print(" de cor " + this.cor);
+        System.out.print(", ponta " + this.ponta);
+        System.out.print(", carga em " + this.carga + "%");
+        if (this.tampada == true)
+            System.out.println(" e está tampada.");
+        else 
+            System.out.println(" e está destampada.");
+    }
+
+    void rabiscar() {
+        if (this.tampada == true)
+            System.out.println("ERRO! Não posso rabiscar.");
+        else 
+            System.out.println("Estou rabiscando.");
+    }
+    
+    void tampar(){
+        this.tampada = true;
+    }
+    
+    void destampar(){
+        this.tampada = false;
+    }
+}
---
+++ papelaria/src/main/java/com/mycompany/papelaria/Papelaria.java
@@ -0,0 +1,15 @@
+package com.mycompany.papelaria;
+
+public class Papelaria {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.cor = "azul";
+        c1.ponta = 0.5f;
+        c1.tampada = false;
+        c1.modelo = "BIC";
+        c1.carga = 80;
+        
+        c1.status();
+    }
+}
